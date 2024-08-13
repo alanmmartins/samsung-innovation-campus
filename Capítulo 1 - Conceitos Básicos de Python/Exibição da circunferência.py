615f8328072c099cc4602cc4baac989e5908132b
alanmmartins <alamunozmartinsa@gmail.com>
2024-08-13 17:19:28 -0300

    q1

---
+++ Capítulo 1 - Conceitos Básicos de Python/Exibição da circunferência.py
@@ -0,0 +1,11 @@
+#Exibição da circunferência e da área do círculo: como funciona
+
+PI = 3.14
+radius = float(input('Informe o raio: '))
+area = PI * radius * radius
+circum = 2.0 * PI * radius
+
+print('Raio do círculo', radius)
+print('Área do círculo', area)
+print('Circunferência do círculo', circum)
+
---
+++ Capítulo 1 - Conceitos Básicos de Python/Tipos de dados numéricos.py
@@ -0,0 +1,28 @@
+#Operadores
+#A tabela abaixo explica o operador de Python e a operação do operador.
+#Operador	Significado	Ação
+#+	Adição	Adicione o operando esquerdo e o operando direito.
+#-	Subtração 	Subtrair o operando direito do operando esquerdo.
+#*	Multiplicação	Multiplique o operando direito e o operando esquerdo.
+#/	Divisão	Dividir o operando esquerdo pelo operando direito. A divisão do Python basicamente retorna o valor do número real.
+#//	Divisão de piso (quociente)	Ao contrário do /, o resultado da divisão é usado para descartar o ponto decimal ou menos e obter apenas a parte inteira.
+#%	Restante	É lido como um operador modulo e não tem nada a ver com a porcentagem que significa relação. Encontra o restante da divisão.
+#**	Potência	Eleva o operando da esquerda à potência do operando da direita. No caso de **0,5, é realizada uma operação de raiz quadrada.
+
+#Exemplo de código utilizando o operador
+#Realizar uma operação usando tipos de dados numéricos. A operação pode ser realizada utilizando dados numéricos e operador +, -, *, /, //, %.
+
+print(100 + 20) # Encontrar adição de valor numérico de 100 e 20
+
+print(100 * 20) # Encontrar multiplicação do valor numérico de 100 e 20
+
+print(100 - 20) # Encontrar subtração de valor numérico de 100 e 20
+
+print(100 / 20) # Dividir o valor numérico de 100 com 20
+
+print(100 // 20) # Dividir o valor numérico de 100 com 20 e encontrar o quociente
+
+print(100 % 20) # Dividir o valor numérico de 100 com 20 e encontrar o resto
+
+print('Hello' * 2)  # Encontrar multiplicação da string 'Hello' e 2
+
