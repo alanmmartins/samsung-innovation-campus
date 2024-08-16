20d57f7b1131ae45b698bf0ff260ffd34bc0dfae
alanmmartins <alamunozmartinsa@gmail.com>
2024-08-16 08:53:33 -0300

    lista III

---
+++ Capítulo 1 - Conceitos Básicos de Python/lista lll - Estrutura Condicional/Ex.py
@@ -0,0 +1,7 @@
+# Solicita ao usuário que insira tres números maiores que zero
+
+numero1 = int(input("Digite o primeiro número maior que zero: "))
+numero2 = int(input("Digite o segundo número maior que zero: "))
+numero3 = int(input("Digite o segundo número maior que zero: "))
+ 
+ 
